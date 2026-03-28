dfa70fc1e5877fc2bf139bfe98dc19f345c6bd1f
Kate <kate.dew.next@gmail.com>
2026-03-28 20:53:30 +0100

    add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { grpcEnv } from './config'
 import { PrismaModule } from './infrastructure/prisma/prisma.module'
 import { RedisModule } from './infrastructure/redis/redis.module'
 import { AuthModule } from './modules/auth/auth.module'
@@ -8,7 +9,7 @@ import { OtpModule } from './modules/otp/otp.module'
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({ isGlobal: true }),
+		ConfigModule.forRoot({ isGlobal: true, load: [grpcEnv] }),
 		PrismaModule,
 		RedisModule,
 		AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 import { AppModule } from './app.module'
+import { AllConfigs } from './config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	const config = app.get(ConfigService<AllConfigs>)
+
+	const host = config.get('grpc.host', { infer: true })
+	const port = config.get('grpc.port', { infer: true })
+	const url = `${host}:${port}`
 
 	app.connectMicroservice<MicroserviceOptions>({
 		transport: Transport.GRPC,
 		options: {
 			package: 'auth.v1',
 			protoPath: 'node_modules/@dewtix/contracts/proto/auth.proto',
-			url: 'localhost:50051',
+			url: url,
 			loader: {
 				keepCase: false,
 				longs: String,
